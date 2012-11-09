3a5f5b77b962404d50159d63d854389864247afc
Mark Tareshawty <tarebyte@gmail.com>
2012-11-08 19:29:59 -0500

    finished git authentication using http auth

--- install.sh
+++ install.sh
@@ -1,3 +1,6 @@
+#-- Store current path --#
+cwdg_setup_path=$(pwd)
+
 #-- Install RVM and Get Ruby Setup --#
 #clear
 #echo Locating Stable RVM
@@ -9,7 +12,7 @@
 #-- Install useful developer tools used from Homebrew --#
 #brew install {git, pbcopy}
 
-#-- Set up Github Auth for HTTP --#
+#-- Set up Git, Github, Auth and Aliases --#
 clear
 echo "Git and Github setup..."
 
@@ -31,3 +34,22 @@ git config --global alias.br "branch"
 git config --global alias.hist "log --pretty=format:\"%h %ad | %s%d [%an]\" --graph --date=short --color"
 git config --global alias.type "cat-file -t"
 git config --global alias.dump "cat-file -p"
+
+# Setup the OSX Keychain
+
+# Download the helper
+curl -s -O http://github-media-downloads.s3.amazonaws.com/osx/git-credential-osxkeychain
+
+# Fix the permissions on the file so it can be run
+chmod u+x git-credential-osxkeychain
+
+# Parse the path of git so that you put it in the correct folder
+which_git=$(which git) # Get the full git path
+which_git_path_length=$(expr ${#which_git} - 4) # Remove the /bin
+git_path=${which_git:0:which_git_path_length} # Create new path from substing
+
+# move the git-credential-osxkeychain to the correct path
+sudo mv git-credential-osxkeychain $(git_path)
+
+# Add the credential to the gitconfig 
+git config --global credential.helper osxkeychain
